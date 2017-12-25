d22524c3a2e4ae67ca45d2bf7be6df6cd32daeb6
Michael Pope <mpope.cr@gmail.com>
2017-12-25 13:22:57 -0800

    Add ORDER BY carrots

--- spec/statements/001_ORDER_BY_carrots_spec.rb
+++ spec/statements/001_ORDER_BY_carrots_spec.rb
@@ -1,49 +1,69 @@
-# RSpec.describe LearnSQL do
-#   before do
-#     LearnSQL.query(%q{
-#       CREATE TABLE carrots (
-#         id serial,
-#         length_mm int,
-#         carotene_mcg int,
-#         PRIMARY KEY (id)
-#       );
-#     })
-#     LearnSQL.query(%q{
-#       INSERT INTO carrots (length_mm,carotene_mcg)
-#       VALUES (177, 8287),
-#              (178, 8286);
-#              (179, 8285);
-#     })
-#   end
-#
-#   after do
-#     LearnSQL.query("DROP TABLE carrots;")
-#   end
-#
-#   describe "ORDER BY" do
-#     it "length_mm using default" do
-#       actual = LearnSQL.query(%q{
-#         SELECT length_mm,carotene_mcg
-#         FROM carrots
-#         ORDER BY length_mm;
-#       })
-#       expected = [
-#         #id,177
-#         [177,8287],
-#         [178,8286],
-#         [177,8285],
-#       ]
-#       expect(actual).to eq(expected)
-#     end
-#
-#     it "color & is_ripe of all bananas" do
-#       actual = LearnSQL.query("SELECT color,is_ripe FROM bananas;")
-#       foo = [
-#         #color,is_ripe
-#         ["green",false],
-#         ["yellow",true],
-#       ]
-#       expect(actual).to eq(foo)
-#     end
-#   end
-# end
+RSpec.describe LearnSQL do
+  before do
+    LearnSQL.query(%q{
+      CREATE TABLE carrots (
+        id serial,
+        length_mm int,
+        carotene_mcg int,
+        PRIMARY KEY (id)
+      );
+    })
+    LearnSQL.query(%q{
+      INSERT INTO carrots (length_mm,carotene_mcg)
+      VALUES (177,8286),
+             (179,8287),
+             (178,8285);
+    })
+  end
+
+  after do
+    LearnSQL.query("DROP TABLE carrots;")
+  end
+
+  describe "ORDER BY" do
+    it "length_mm using default order" do
+      actual = LearnSQL.query(%q{
+        SELECT length_mm,carotene_mcg
+        FROM carrots
+        ORDER BY length_mm;
+      })
+      expected = [
+        # length_mm,carotene_mcg
+        [177,8286],
+        [178,8285],
+        [179,8287],
+      ]
+      expect(actual).to eq(expected)
+    end
+
+    it "length_mm ascending" do
+      actual = LearnSQL.query(%q{
+        SELECT length_mm,carotene_mcg
+        FROM carrots
+        ORDER BY length_mm ASC;
+      })
+      expected = [
+        # length_mm,carotene_mcg
+        [177,8286],
+        [178,8285],
+        [179,8287],
+      ]
+      expect(actual).to eq(expected)
+    end
+
+    it "length_mm descending" do
+      actual = LearnSQL.query(%q{
+        SELECT length_mm,carotene_mcg
+        FROM carrots
+        ORDER BY length_mm DESC;
+      })
+      expected = [
+        # length_mm,carotene_mcg
+        [179,8287],
+        [178,8285],
+        [177,8286],
+      ]
+      expect(actual).to eq(expected)
+    end
+  end
+end
