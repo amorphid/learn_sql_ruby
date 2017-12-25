8350aa83a7327ec0095030f9a4f0070d1054b643
Michael Pope <mpope.cr@gmail.com>
2017-12-25 13:49:54 -0800

    Add DISTINCT coconuts

---
+++ spec/statements/002_DISTINCT_coconuts_spec.rb
@@ -0,0 +1,84 @@
+RSpec.describe LearnSQL do
+  before do
+    LearnSQL.query(%q{
+      CREATE TABLE coconuts (
+        id serial,
+        weight_lbs int,
+        milk_oz int
+      );
+    })
+    LearnSQL.query(%q{
+      INSERT INTO coconuts (weight_lbs,milk_oz)
+      VALUES (10,8),
+             (10,9),
+             (10,9),
+             (11,8),
+             (11,10);
+    })
+  end
+
+  after do
+    LearnSQL.query("DROP TABLE coconuts;")
+  end
+
+  describe "DISTINCT" do
+    it "coconuts" do
+      actual = LearnSQL.query(%q{
+        SELECT DISTINCT weight_lbs,milk_oz
+        FROM coconuts
+        ORDER BY weight_lbs,milk_oz;
+      })
+      expected = [
+        # weight_lbs,milk_oz
+        [10,8],
+        [10,9],
+        [11,8],
+        [11,10],
+      ]
+      expect(actual).to eq(expected)
+    end
+
+    it "coconuts weights" do
+      actual = LearnSQL.query(%q{
+        SELECT DISTINCT weight_lbs
+        FROM coconuts
+        ORDER BY weight_lbs;
+      })
+      expected = [
+        # weight_lbs
+        [10],
+        [11],
+      ]
+      expect(actual).to eq(expected)
+    end
+
+    it "coconuts on weight_lbs" do
+      actual = LearnSQL.query(%q{
+        SELECT DISTINCT ON (weight_lbs) weight_lbs,milk_oz
+        FROM coconuts
+        ORDER BY weight_lbs,milk_oz;
+      })
+      expected = [
+        # weight_lbs,milk_oz
+        [10,8],
+        [11,8],
+      ]
+      expect(actual).to eq(expected)
+    end
+
+    it "coconuts on milk_oz" do
+      actual = LearnSQL.query(%q{
+        SELECT DISTINCT ON (milk_oz) weight_lbs,milk_oz
+        FROM coconuts
+        ORDER BY milk_oz;
+      })
+      expected = [
+        # weight_lbs,milk_oz
+        [10,8],
+        [10,9],
+        [11,10],
+      ]
+      expect(actual).to eq(expected)
+    end
+  end
+end
