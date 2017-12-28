17d4f7ebeaef5a2246d732811011792752bc7a10
Michael Pope <mpope.cr@gmail.com>
2017-12-28 09:15:13 -0800

    Add SUBQUERY raisins

---
+++ spec/statements/017_SUBQUERY_apricots_spec.rb
@@ -0,0 +1,44 @@
+RSpec.describe LearnSQL do
+  before do
+    LearnSQL.query(%q{
+      CREATE TABLE raisins (
+        id SERIAL,
+        weight_g INT,
+        PRIMARY KEY (id)
+      );
+    })
+
+    LearnSQL.query(%q{
+      INSERT INTO raisins (weight_g)
+      VALUES (1),
+             (2),
+             (3),
+             (3),
+             (4);
+    })
+  end
+
+  after do
+    LearnSQL.query("DROP TABLE raisins;")
+  end
+
+  describe "SUBQUERY" do
+    it "select raisins which weigh more than average" do
+      actual = LearnSQL.query(%q{
+        SELECT id,weight_g
+        FROM raisins
+        WHERE weight_g > (
+          SELECT AVG(weight_g)
+          FROM raisins
+        );
+      })
+      expected = [
+        # id,weight_g
+        [3,3],
+        [4,3],
+        [5,4],
+      ]
+      expect(actual).to eq(expected)
+    end
+  end
+end
