db45bc138479ed0243e2de34dbc14e635a649469
Michael Pope <mpope.cr@gmail.com>
2017-12-27 20:15:07 -0800

    Add GROUP BY cherries

---
+++ spec/statements/012_GROUP_BY_cherries_spec.rb
@@ -0,0 +1,42 @@
+RSpec.describe LearnSQL do
+  before do
+    LearnSQL.query(%q{
+      CREATE TABLE cherries (
+        id SERIAL,
+        cultivar CHARACTER VARYING,
+        is_sweet BOOLEAN,
+        PRIMARY KEY (id)
+      );
+    })
+    LearnSQL.query(%q{
+      INSERT INTO cherries (cultivar,is_sweet)
+      VALUES ('Accolade',TRUE),
+             ('Autumnalis',FALSE),
+             ('Pandora',TRUE),
+             ('Pendula Rosea',FALSE),
+             ('Tibetan cherry',TRUE),
+             ('Kiku-shidare-zakura',TRUE);
+    })
+  end
+
+  after do
+    LearnSQL.query("DROP TABLE cherries;")
+  end
+
+  describe "GROUP BY" do
+    it "is_sweet w/ count" do
+      actual = LearnSQL.query(%q{
+        SELECT is_sweet,COUNT (id) AS count
+        FROM cherries
+        GROUP BY is_sweet
+        ORDER BY is_sweet DESC;
+      })
+      expected = [
+        # is_sweet,count
+        [true,4],
+        [false,2],
+      ]
+      expect(actual).to eq(expected)
+    end
+  end
+end
